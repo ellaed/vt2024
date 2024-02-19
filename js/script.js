21e4d7efe68840c63e28c0fc5633276092d820cc
ellaed <ella.edvardsson@elev.ga.ntig.se>
2024-02-19 11:59:58 +0100

    ....

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // Denna fil skall innehålla JavaScript (JS)
+const hamburger = document.querySelector("#meny");
+const nav = document.querySelector("nav");
+
+meny.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    nav.classList.toggle("active");
+}
